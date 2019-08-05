fbc7f7ec8448d45308f1322d12b78b05824acd08
Alexismn <alexismn86@gmail.com>
2019-08-05 15:18:21 +0200

    using redux devtools extensions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import Thunk from 'redux-thunk';
 
 import App from './containers/App';
@@ -11,7 +11,10 @@ import reducers from './reducers'
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 
-const store = createStore(reducers, applyMiddleware(Thunk));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(Thunk)));
+//const store = createStore(reducers, applyMiddleware(Thunk));
 
 //BrowserRouter per a fer servir el router i poder gestionar l'historic
 ReactDOM.render(
